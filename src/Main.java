504dd2ee240f2f2282b911cb9e9c872216261828
Stanislav <stanislav.petr06@gmail.com>
2024-03-18 09:00:09 +0100

    removed Standajekokot sout

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("Standajekokot");
+
         System.out.println("Zkouska na commit");
     }
 }
